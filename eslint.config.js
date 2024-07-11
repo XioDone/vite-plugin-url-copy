b1a7a728633034df7b6dc2066473cd1841ff69f2
Doxo <gentleDone@outlook.com>
2024-07-12 02:08:56 +0800

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,45 +1,8 @@
-import antfu from '@antfu/eslint-config'
+import linter from '@antfu/eslint-config'
+import xiodone from '@xiodone/eslint-config'
 
-const config = antfu(
-  {
-    ignores: [],
-  },
-  {
-    rules: {
-      'curly': ['error', 'all'],
-      'no-console': 'off',
-      'array-callback-return': 0,
-      'prefer-promise-reject-errors': ['error', { allowEmptyReject: true }],
-    },
-  },
-  {
-    rules: {
-      'style/brace-style': ['error', '1tbs'],
-      'style/quotes': ['error', 'single', { avoidEscape: true }],
-      'style/arrow-parens': ['error', 'as-needed'],
-    },
-  },
-  {
-    rules: {
-      'ts/ban-types': [2, { types: { Function: false } }],
-    },
-  },
-  {
-    rules: {
-      'antfu/top-level-function': 'off',
-    },
-  },
-  {
-    rules: {
-      'jsdoc/no-defaults': 'off',
-      'jsdoc/require-returns-description': 'off',
-    },
-  },
-  {
-    rules: {
-      'node/prefer-global/process': 'off',
-    },
-  },
+const config = linter(
+  ...xiodone(),
 )
 
 export default config
